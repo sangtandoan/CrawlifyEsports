56f64071ee8bc7758c8156769034402ff05999f2
FrostJ143 <sangvaminh11497@gmail.com>
2024-09-15 20:57:53 +0700

    add games, tier, upcoming, ongoing flags

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"slices"
@@ -17,21 +18,36 @@ type Tournament struct {
 	Name      string `json:"name"`
 	StartDate string `json:"start_date"`
 	EndDate   string `json:"end_date"`
+	Tier      string `json:"tier"`
 }
 
-const (
-	lol  = "League of Legends"
-	cs2  = "Counter Strike 2"
-	val  = "Valorant"
-	pubg = "PUBG"
-)
+type Match struct {
+	TournamentName string   `json:"tournament_name"`
+	TeamLeft       string   `json:"team_left"`
+	TeamRight      string   `json:"team_right"`
+	Links          []string `json:"links"`
+}
+
+type CommandLineArgs struct {
+	tier        string
+	chosenGames []string
+	ongoing     bool
+	upcoming    bool
+}
 
 var (
 	games = map[string]string{
-		lol:  "https://liquipedia.net/leagueoflegends/Main_Page",
-		cs2:  "https://liquipedia.net/counterstrike/Main_Page",
-		val:  "https://liquipedia.net/valorant/Main_Page",
-		pubg: "https://liquipedia.net/pubg/Main_Page",
+		"lol":  "https://liquipedia.net/leagueoflegends/Main_Page",
+		"cs2":  "https://liquipedia.net/counterstrike/Main_Page",
+		"val":  "https://liquipedia.net/valorant/Main_Page",
+		"pubg": "https://liquipedia.net/pubg/Main_Page",
+	}
+
+	names = map[string]string{
+		"lol":  "League of Legends",
+		"cs2":  "Counter Strike 2",
+		"val":  "Valorant",
+		"pubg": "PUBG",
 	}
 
 	// channels = map[string]chan Tournament{
@@ -42,20 +58,38 @@ var (
 	// }
 
 	tournaments = make(map[string][]Tournament) // map[string][]tournament{}, init empty map
+	matches     = make(map[string][]Match)
 )
+
+// gameSlice implement flag.Value interface
+type gamesSlice []string
+
+// flag.Value interface method
+func (gs *gamesSlice) String() string {
+	return strings.Join(*gs, ",")
+}
+
+// flag.Value interface method
+func (gs *gamesSlice) Set(value string) error {
+	// *gs = append(*gs, value) uses this if want to have multiple flag to set this value
+	*gs = strings.Split(value, ",") // uses this if just want to have one flag to set this value using csv
+	return nil
+}
 
 func main() {
 	start := time.Now()
 	// closeChan := make(chan bool)
 	// done := make(chan bool)
 
+	args := parseCommandLineArgs()
+
 	wg := &sync.WaitGroup{}
-	for key, value := range games {
+	for _, game := range args.chosenGames {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
 			// scrapingForGame(value, channels[key], done)
-			scrapingForGame(value, tournaments, key)
+			scrapingForGame(games[game], tournaments, game, args, matches)
 		}()
 	}
 
@@ -94,20 +128,10 @@ func main() {
 	//
 
 	// Sort tournaments based on StartDate
-	for _, tournament := range tournaments {
-		slices.SortFunc(tournament, func(a, b Tournament) int {
-			startDateA, _ := time.Parse("02-01-2006", a.StartDate)
-			startDateB, _ := time.Parse("02-01-2006", b.StartDate)
-			if startDateA.Before(startDateB) {
-				return -1
-			} else {
-				return 1
-			}
-		})
-	}
+	sortByStartDate()
 
-	for key, tournament := range tournaments {
-		renderToTable(key, tournament)
+	for key := range tournaments {
+		renderToTable(key, tournaments[key], matches[key])
 		fmt.Println()
 	}
 
@@ -186,14 +210,26 @@ func main() {
 // 	done <- true
 // }
 
-func scrapingForGame(link string, tournaments map[string][]Tournament, key string) {
-	collector := colly.NewCollector(colly.Async(true))
+func scrapingForGame(link string, tournaments map[string][]Tournament, key string, args CommandLineArgs, matches map[string][]Match) {
+	collector := colly.NewCollector(colly.Async(true), colly.CacheDir(""))
+
+	collector.OnRequest(func(r *colly.Request) {
+		r.Headers.Set("Cache-Control", "no-cache")
+		r.Headers.Set("Pragma", "no-cache")
+		r.Headers.Set("Expires", "0")
+
+		// Add a timestamp to the URL to bypass cache
+		r.URL.RawQuery += "&t=" + fmt.Sprint(time.Now().Unix())
+	})
 
 	collector.OnError(func(r *colly.Response, err error) {
 		fmt.Println(err)
 	})
 
 	collector.OnHTML("ul#tournaments-menu-upcoming", func(h *colly.HTMLElement) {
+		if !args.upcoming {
+			return
+		}
 		// wg := sync.WaitGroup{}
 		h.ForEach("a.dropdown-item", func(_ int, el *colly.HTMLElement) {
 			// wg.Add(1)
@@ -209,6 +245,9 @@ func scrapingForGame(link string, tournaments map[string][]Tournament, key strin
 	})
 
 	collector.OnHTML("ul#tournaments-menu-ongoing", func(h *colly.HTMLElement) {
+		if !args.ongoing {
+			return
+		}
 		// wg := sync.WaitGroup{}
 
 		h.ForEach("a.dropdown-item", func(_ int, el *colly.HTMLElement) {
@@ -225,12 +264,12 @@ func scrapingForGame(link string, tournaments map[string][]Tournament, key strin
 		// wg.Wait()
 	})
 
-	collector.OnHTML("div.fo-nttax-infobox", func(h *colly.HTMLElement) {
+	collector.OnHTML("div.fo-nttax-infobox-wrapper", func(h *colly.HTMLElement) {
 		tournament := Tournament{}
 		tournament.Name = h.ChildText("div:nth-child(1) > div.infobox-header")
-		if tournament.Name == "Upcoming Matches" {
-			return
-		}
+		// if tournament.Name == "Upcoming Matches" {
+		// 	return
+		// }
 
 		tournament.Name = strings.ReplaceAll(tournament.Name, "[e][h]", "")
 
@@ -244,15 +283,43 @@ func scrapingForGame(link string, tournaments map[string][]Tournament, key strin
 			case "Date:":
 				tournament.StartDate = formatTime(el.ChildText(selectorForDate))
 				tournament.EndDate = formatTime(el.ChildText(selectorForDate))
+			case "Liquipedia Tier:":
+				tournament.Tier = el.ChildText("a")
 			}
 		})
 
-		tournaments[key] = append(tournaments[key], tournament)
+		if filterByTier(args.tier, tournament) {
+			tournaments[key] = append(tournaments[key], tournament)
+		}
+		getMatchesFromTournament(tournament.Name, key, h)
 	})
 
 	collector.Visit(link)
 	// wg.Wait()
 	collector.Wait()
+}
+
+func getMatchesFromTournament(tournament string, key string, h *colly.HTMLElement) {
+	h.ForEach("table", func(_ int, el *colly.HTMLElement) {
+		startTime := el.ChildText("span.match-countdown")
+		startTime = strings.TrimSpace(strings.ReplaceAll(startTime, "UTC", ""))
+		t, _ := time.Parse("January 02, 2006 - 15:04", startTime)
+		loc, _ := time.LoadLocation("Local")
+		t = t.In(loc)
+		fmt.Println(t.Format("15:04 02-01-2006"))
+		fmt.Println(el.ChildText("td.team-left span.team-template-text"))
+		match := Match{TournamentName: tournament}
+		match.TeamLeft = el.ChildText("td.team-left span.team-template-text")
+		match.TeamRight = el.ChildText("td.team-right span.team-template-text")
+		match.Links = []string{func() string {
+			if attr, ok := el.DOM.Find("span.match-countdown + a:nth-child(1)").Attr("href"); ok {
+				return strings.TrimSpace(attr)
+			}
+			return ""
+		}()}
+
+		matches[key] = append(matches[key], match)
+	})
 }
 
 // formatTime change default format from 2006-01-02 to 02-01-2006
@@ -275,21 +342,34 @@ func formatTime(oldTime string) string {
 	return t.Format("02-01-2006")
 }
 
-func renderToTable(gameName string, tournaments []Tournament) {
+func renderToTable(gameName string, tournaments []Tournament, matches []Match) {
+	t := createTableWriter(gameName)
+	m := createTableWriter(gameName)
+
+	m.AppendHeader(table.Row{"Tournament", "Versus", "Links"})
+
+	t.AppendHeader(table.Row{"Tournament", "Start Date", "End Date", "Tier"})
+
+	for _, tournament := range tournaments {
+		t.AppendRow(table.Row{tournament.Name, tournament.StartDate, tournament.EndDate, tournament.Tier})
+	}
+
+	for _, match := range matches {
+		m.AppendRow(table.Row{match.TournamentName, match.TeamLeft + " vs " + match.TeamRight, match.Links})
+	}
+
+	t.Render()
+	m.Render()
+}
+
+func createTableWriter(gameName string) table.Writer {
 	t := table.NewWriter()
 	t.SetOutputMirror(os.Stdout)
 	t.SetAutoIndex(true)
-	t.SetTitle(gameName)
+	t.SetTitle(names[gameName])
 	// Use t.Style().something.something to style a specific thing
 	// Use t.SetStyle() to style for a whole table
 	t.Style().Title.Align = text.AlignCenter
-
-	t.AppendHeader(table.Row{"Tournament", "Start Date", "End Date"})
-
-	for _, tournament := range tournaments {
-		t.AppendRow(table.Row{tournament.Name, tournament.StartDate, tournament.EndDate})
-	}
-
 	t.SetColumnConfigs([]table.ColumnConfig{
 		{
 			// Specify name of column to apply config to
@@ -298,5 +378,67 @@ func renderToTable(gameName string, tournaments []Tournament) {
 		},
 	})
 
-	t.Render()
+	return t
+}
+
+func sortByStartDate() {
+	for _, tournament := range tournaments {
+		slices.SortFunc(tournament, func(a, b Tournament) int {
+			startDateA, _ := time.Parse("02-01-2006", a.StartDate)
+			startDateB, _ := time.Parse("02-01-2006", b.StartDate)
+			if startDateA.Before(startDateB) {
+				return -1
+			} else {
+				return 1
+			}
+		})
+	}
+}
+
+func parseCommandLineArgs() CommandLineArgs {
+	// Using csv to parse chosen games
+	// chosenGamesFlag := flag.String("games", "", "select which games want to crawl")
+	// flag.Parse()
+	// chosenGames := strings.Split(*chosenGamesFlag, ",")
+	// fmt.Println(chosenGames)
+
+	// Using flag.Var and custom implementation of flag.Value to parse
+	var chosenGames gamesSlice
+	flag.Var(&chosenGames, "games", "select which games want to crawl")
+	tier := flag.String("tier", "b", "slect which tiers to crawl")
+	ongoing := flag.Bool("ongoing", true, "select ongoing tournaments")
+	upcoming := flag.Bool("upcoming", true, "select upcoming tournaments")
+	flag.Parse()
+
+	if len(chosenGames) == 0 {
+		for key := range names {
+			chosenGames = append(chosenGames, key)
+		}
+	}
+
+	args := CommandLineArgs{
+		chosenGames: chosenGames,
+		tier:        *tier,
+		ongoing:     *ongoing,
+		upcoming:    *upcoming,
+	}
+
+	return args
+}
+
+// filterByTier filters tournaments based on their tier, return true if tournament matches the tier
+func filterByTier(tier string, tournament Tournament) bool {
+	cur_tier := strings.ToLower(strings.ReplaceAll(tournament.Tier, "-Tier", ""))
+
+	if cur_tier != "s" {
+		if tier == "s" {
+			return false
+		} else {
+			if cur_tier > tier {
+				return false
+			}
+		}
+	}
+
+	return true
 }
--- go.mod
+++ go.mod
@@ -13,14 +13,20 @@ require (
 	github.com/antchfx/htmlquery v1.3.2 // indirect
 	github.com/antchfx/xmlquery v1.4.1 // indirect
 	github.com/antchfx/xpath v1.3.1 // indirect
+	github.com/deckarep/golang-set/v2 v2.6.0 // indirect
+	github.com/go-jose/go-jose/v3 v3.0.3 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/playwright-community/playwright-go v0.4700.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842 // indirect
 	golang.org/x/net v0.29.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
 	golang.org/x/text v0.18.0 // indirect
